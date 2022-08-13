1a4f058b0590ff1e68baadd866574b055cc96a24
Shinu Mathew <shinu@makolet.in>
2022-08-13 21:01:19 +0530

    Linked list problems

---
+++ src/com/scalar/ds/array/subarrays/FindSubarrays.java
@@ -0,0 +1,29 @@
+package com.scalar.ds.array.subarrays;
+
+public class FindSubarrays {
+	/*
+	 * Given an array find the all the subarrays
+	 * */
+	public static void main(String[] args) {
+		
+		int[] arr = {2, -1, 3, 1};
+		getSubarrays(arr);
+		// TC : O(n^3) => It cannot be optimized
+		
+	}
+	
+	private static void getSubarrays(int arr[]) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i; j< arr.length; j++) {				
+				System.out.print("["+i+", "+j+"] => ");
+				System.out.print("{ ");
+				for(int k = i; k < j+1; k++) {
+					System.out.print(arr[k]+" ");
+				}
+				System.out.print("}");
+				System.out.println("");
+			}
+		}
+	}
+
+}
---
+++ src/com/scalar/ds/array/twodmatrix/PrintDiagonalsFor2DMatrix.java
@@ -0,0 +1,38 @@
+package com.scalar.ds.array.twodmatrix;
+
+public class PrintDiagonalsFor2DMatrix {
+	
+	/**
+	 * Given a N x M 2D matrix print the left to right and right to left diagonals
+	 */
+	public static void main(String[] args) {
+		int[][] arr = { { 2, 3, -2, 0 }, { 6, 2, 4, 1 }, { -4, 1, 5, 3 }, { 0, -5, 2, 8 } };
+//		leftToRight(arr);
+		rightToLeft(arr);
+		
+	}
+
+	private static void leftToRight(int[][] arr) {		
+		int i = 0;
+		int N = arr.length;
+		int M = arr[0].length;
+		System.out.println("Left To Right");
+		
+		while(i < N) {
+			System.out.println(arr[i][i]);
+			i++;
+		}
+	}
+	
+	private static void rightToLeft(int[][] arr) {
+		int i = 0;
+		int j = arr[0].length-1;
+		
+		while(i < arr.length  && j >= 0) {
+			System.out.println(arr[i][j]);
+			i++;
+			j--;
+		}
+	}
+
+}
