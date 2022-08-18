d8106e1bbba875bffe0cd869a19ccb54deb7b5de
Shinu Mathew <shinu@makolet.in>
2022-08-18 23:31:50 +0530

    Stack implementation in progress

---
+++ src/com/scalar/ds/stack/Node.java
@@ -0,0 +1,13 @@
+package com.scalar.ds.stack;
+
+public class Node {
+	
+	int data;
+	Node next;
+	
+	Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+}
---
+++ src/com/scalar/ds/stack/StackImplementation.java
@@ -0,0 +1,14 @@
+package com.scalar.ds.stack;
+
+public class StackImplementation {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	private void createStack(int[] arr) {
+		
+	}
+
+}
