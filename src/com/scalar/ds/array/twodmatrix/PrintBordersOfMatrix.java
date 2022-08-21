d5d5a76bd34bed2c7563ef59d53bf55c84ee9e60
Shinu Mathew <shinu@makolet.in>
2022-08-21 11:09:57 +0530

    2D matrix

--- src/com/scalar/ds/array/twodmatrix/PrintBordersOfMatrix.java
+++ src/com/scalar/ds/array/twodmatrix/PrintBordersOfMatrix.java
@@ -8,38 +8,60 @@ public class PrintBordersOfMatrix {
 	 */
 
 	public static void main(String[] args) {
-		int arr[][] = { 
-				{ 1,  2,  3,  4,  5 }, 
-				{ 6,  7,  8,  9,  10 }, 
-				{ 11, 12, 13, 14, 15 }, 
-				{ 16, 17, 18, 19, 20 },
-				{ 21, 22, 23, 24, 25 } 
-			};
+		int arr[][] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 },
+				{ 21, 22, 23, 24, 25 } };
 		printBorders(arr);
+		System.out.println("===== =");
+		printBordersCleaner(arr);
 	}
-	
+
 	static void printBorders(int[][] arr) {
 		int N = arr.length;
-		
-		// print 0 to N-2 columns of the 0th row 
-		for(int i = 0; i < N - 1; i++) {
+
+		// print 0 to N-2 columns of the 0th row
+		for (int i = 0; i < N - 1; i++) {
 			System.out.println(arr[0][i]);
-			
+
 		}
-		
-		// print 0 to N-2 rows of the N-1th column 
-		for(int i = 0; i < N - 1; i++) {
-			System.out.println(arr[i][N-1]);
+
+		// print 0 to N-2 rows of the N-1th column
+		for (int i = 0; i < N - 1; i++) {
+			System.out.println(arr[i][N - 1]);
 		}
-		
-		// print N-1 to 1 column of the N-1th row 
-		for(int i = N - 1; i > 0; i--) {
-			System.out.println(arr[N-1][i]);
+
+		// print N-1 to 1 column of the N-1th row
+		for (int i = N - 1; i > 0; i--) {
+			System.out.println(arr[N - 1][i]);
 		}
-		
-		// print 0 to N-1 row of the 0th column 
-		for(int i = N - 1; i > 0; i--) {
+
+		// print 0 to N-1 row of the 0th column
+		for (int i = N - 1; i > 0; i--) {
 			System.out.println(arr[i][0]);
+		}
+	}
+
+	static void printBordersCleaner(int[][] arr) {
+		int i = 0, j = 0;
+		int n = arr.length;
+
+		for (int k = 0; k < n - 1; k++) {
+			System.out.println(arr[i][j]);
+			j++;
+		}
+
+		for (int k = 0; k < n - 1; k++) {
+			System.out.println(arr[i][j]);
+			i++;
+		}
+
+		for (int k = 0; k < n - 1; k++) {
+			System.out.println(arr[i][j]);
+			j--;
+		}
+
+		for (int k = 0; k < n - 1; k++) {
+			System.out.println(arr[i][j]);
+			i--;
 		}
 	}
 }
