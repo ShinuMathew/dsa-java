7a6bea71b08623fd7d02a01bc48db258f4cf57fc
Shinu Mathew <shinu@makolet.in>
2022-09-10 01:08:58 +0530

    NonOverlappingIntervals, ArrayResultWithQueries, ArrayResultWithQueries2, FindKin2DMatrix - Advanced Arrays 3

---
+++ src/com/scalar/ds/array/prefixsum/ArrayResultWithQueries2.java
@@ -0,0 +1,39 @@
+package com.scalar.ds.array.prefixsum;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ArrayResultWithQueries2 {
+	
+	/**
+	 * Given an array of length N such that each arr[i] = 0 initially. 
+	 * Return the final array after performing all queries
+	 * Queries (i, j, x) --> Add x to all numbers from i to j 
+	 */
+	public static void main(String[] args) {
+		int[] arr = {0, 0, 0, 0, 0, 0, 0};
+		int[][] queries = {{1, 3, 2}, {2, 5, 3}, {2, 4, -1}};
+		int[] result = executeQueries(queries, arr);
+		Arrays.stream(result).boxed().collect(Collectors.toList()).forEach(System.out::println);
+	}
+	
+	private static int[] executeQueries(int[][] queries, int[] arr) {
+		for(int q = 0; q < queries.length; q++) {
+			int i = queries[q][0];
+			int j = queries[q][1];
+			int x = queries[q][2];
+			arr[i] += x;
+			if(j<arr.length-1)
+				arr[j+1] -= x;			
+		}
+		
+		int[] psArr = new int[arr.length];
+		psArr[0] = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			psArr[i] = psArr[i-1] + arr[i];
+		}
+		return psArr;
+	}	
+	
+
+}
---
+++ src/com/scalar/ds/array/twodmatrix/FindKin2DMatrix.java
@@ -0,0 +1,44 @@
+package com.scalar.ds.array.twodmatrix;
+
+public class FindKin2DMatrix {
+
+	/**
+	 * Given a 2D matrix and a value "k". Find if "k" exists in the matrix.
+	 */
+	public static void main(String[] args) {
+		
+		int[][] A = {
+				{-10, -5, -2,  2,  4,  7},
+				{ -7, -4, -1,  3,  6,  9},
+				{ -2,  3,  5,  7,  10, 14},
+				{  3,  6,  8,  11, 14, 17},
+				{  7,  11, 12, 15, 19, 20},
+				{ 10,  16, 18, 20, 24, 29},
+		};
+		int k = 12;
+		System.out.println(findK(A, k));
+	}
+	
+	private static String findK(int[][] A, int k) {
+		int N = A.length;
+		int M = A[0].length;
+		int i = 0;
+		int j = M-1;
+		
+		while(i < N && j >= 0) {
+			
+			if(A[i][j] == k) {
+				return i+", "+j;
+			}
+			
+			if(A[i][j] < k) {
+				i++;				
+			} else {
+				j--;
+			}			
+			
+		}
+		return "null";
+		
+	}
+}
